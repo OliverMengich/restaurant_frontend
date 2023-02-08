77d00e63d69c59b7b590f6761e312a1519f2f3f5
OliverMengich <kemeioliver2000@gmail.com>
2023-02-08 22:20:29 +0300

    Modifies background opacity for PageInfo. Separates DishesItem to a different component

--- src/components/DishesList/DishesList.component.tsx
+++ src/components/DishesList/DishesList.component.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import dishesListStyles from './DishesList.module.css';
-import Image, { StaticImageData } from 'next/image';
+import DishItem from '../DishItem/DishItem.component';
 interface Dish{
     id: number,
     imageUrl: string,
     name: string,
     price: number,
     description: string,
+    rating: number
 }
 interface DishesListProps{
     dishes: Dish[]
@@ -18,35 +19,36 @@ function DishesListComponent({dishes}: DishesListProps) {
                 <h1>Highly Rated Dishes</h1>
                 <div className={dishesListStyles["dishes-container"]}>
                     {dishes.map((dish) => (
-                        <div key={dish.id} className={dishesListStyles["dish"]}>
-                            <Image
-                                src={dish.imageUrl}
-                                alt="image1"
-                                width={300}
-                                height={300}
-                            />
-                            <div className={dishesListStyles["dish-info"]}>
-                                <h3 className={dishesListStyles["title"]}>
-                                    {dish.name}
-                                </h3>
-                                <p>${dish.price}</p>
-                            </div>
-                            <p>{dish.description}</p>
-                            <div className={dishesListStyles.dishes_action}>
-                                <button className={dishesListStyles.btn}>
-                                    Order
-                                </button>
-                                <button className={dishesListStyles["cart-btn"]}>
-                                    <Image
-                                        src="https://cdn-icons-png.flaticon.com/512/3737/3737372.png"
-                                        alt="star"
-                                        width={20}
-                                        height={20}
-                                    />
-                                    Add to Cart
-                                </button>
-                            </div>
-                        </div>
+                        <DishItem key={dish.id} dish={dish} />
+                        // <div key={dish.id} className={dishesListStyles["dish"]}>
+                        //     <Image
+                        //         src={dish.imageUrl}
+                        //         alt="image1"
+                        //         width={300}
+                        //         height={300}
+                        //     />
+                        //     <div className={dishesListStyles["dish-info"]}>
+                        //         <h3 className={dishesListStyles["title"]}>
+                        //             {dish.name}
+                        //         </h3>
+                        //         <p>${dish.price}</p>
+                        //     </div>
+                        //     <p>{dish.description}</p>
+                        //     <div className={dishesListStyles.dishes_action}>
+                        //         <button className={dishesListStyles.btn}>
+                        //             Order
+                        //         </button>
+                        //         <button className={dishesListStyles["cart-btn"]}>
+                        //             <Image
+                        //                 src="https://cdn-icons-png.flaticon.com/512/3737/3737372.png"
+                        //                 alt="star"
+                        //                 width={20}
+                        //                 height={20}
+                        //             />
+                        //             Add to Cart
+                        //         </button>
+                        //     </div>
+                        // </div>
                     ))}
                 </div>
             </div>

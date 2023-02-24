b1566fe082d8d990d4390a551c3f2ba8706fc328
OliverMengich <kemeioliver2000@gmail.com>
2023-02-24 07:09:03 +0300

    Building API requests

--- src/pages/api/dishes.ts
+++ src/pages/api/dishes.ts
@@ -1,13 +1,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { DishOutput } from "./typeDefs";
-import { getDishes } from "./controllers/dishes.controller";
+import { createADish, deleteADish, getDishes } from "./controllers/dishes.controller";
 interface DishesResponse {
     dishes: DishOutput[];
     status: "success" | "error";
 }
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse<DishOutput[]>) {
-    const dishes = await getDishes(req, res);
-    console.log(dishes);
-    return res.status(200).send(dishes);
+    const method = req.method;
+    switch (method) {
+        case 'GET':
+            const dishes = await getDishes(req, res);
+            console.log(dishes);
+            res.status(200).send(dishes);
+            break;
+        case 'POST':
+            const newDish = await createADish(req,res);
+            console.log(newDish);
+            res.status(200).send(newDish);
+            break;
+        case 'DELETE':
+            const deletedDish = await deleteADish(req,res);
+            console.log(deletedDish);
+            res.send(newDish)
+            break;
+        default:
+            res.status(405).end("Method not allowed")
+            break;
+    }
+    
 }

1ee91fd41907cf98e6f47dc34857f19d3607e3b8
OliverMengich <kemeioliver2000@gmail.com>
2023-03-05 17:49:00 +0300

    Fixes ssl required issue

--- src/pages/api/database/db.js
+++ src/pages/api/database/db.js
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize";
 import config from '../config/index.js';
 const sequelize = new Sequelize(config.DB_URL?? '',{
     logging: false,
+    ssl: true,
 })
 const instance = async ()=> await sequelize.authenticate().then(()=>console.log('Connected to Database')).catch(err=>console.log("Error"))
 instance();
